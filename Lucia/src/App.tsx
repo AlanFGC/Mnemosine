00ef81035d892d594edfd6a7441487e3acd60dc9
Alan Garcia <alan_fgc@outlook.com>
2023-10-17 12:49:56 -0700

    Frontend updates to the linter

--- Lucia/src/App.tsx
+++ Lucia/src/App.tsx
@@ -2,12 +2,10 @@ import { Button } from 'antd';
 
 function App() {
   return (
-    <>
-      <div className="App">
-          <Button type="primary">Button</Button>
-        </div>
-    </>
-  )
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
