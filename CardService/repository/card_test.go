b011827b39054736c1dedd57d10c18877c076ef2
Alan Garcia <alan_fgc@outlook.com>
2023-10-27 16:56:35 -0700

    Added test for cards.go

--- CardService/repository/card_test.go
+++ CardService/repository/card_test.go
@@ -129,7 +129,130 @@ func TestInsertOneCardIntegration(t *testing.T) {
 	})
 }
 
-func TestGetCardsByUsernameIntegration(t *testing.T) {
+func TestGetCardById(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mt.Close()
+
+	mt.Run("Get cards by Id", func(mt *mtest.T) {
+
+		primitiveIDs := make([]primitive.ObjectID, 3)
+		for i := 0; i < 3; i++ {
+			primitiveIDs[i] = primitive.NewObjectID()
+		}
+
+		responses := []bson.D{
+			{
+				{"_id", primitiveIDs[0]},
+				{"username", TESTUSERNAME1},
+				{"title", "Sample Card 1"},
+				{"text", "This is a sample flash card."},
+				{"answers", bson.A{
+					bson.D{
+						{"field", 1},
+						{"answers", bson.A{TESTANSWER1}},
+						{"questionType", Model.SingleAnswer},
+					},
+				}},
+				{"media", bson.A{"sample.jpg"}},
+				{"lang", bson.A{"en", "es"}},
+				{"topics", bson.A{"sample"}},
+				{"dateCreated", primitive.NewDateTimeFromTime(time.Now())},
+			},
+			{
+				{"_id", primitiveIDs[1]},
+				{"username", TESTUSERNAME2},
+				{"title", "Sample Card 2"},
+				{"text", "This is a sample flash card."},
+				{"answers", bson.A{
+					bson.D{
+						{"field", 2},
+						{"answers", bson.A{TESTANSWER2}},
+						{"questionType", Model.SingleAnswer},
+					},
+				}},
+				{"media", bson.A{"sample.jpg"}},
+				{"lang", bson.A{"en", "es"}},
+				{"topics", bson.A{"sample"}},
+				{"dateCreated", primitive.NewDateTimeFromTime(time.Now())},
+			},
+			{
+				{"_id", primitiveIDs[2]},
+				{"username", TESTUSERNAME3},
+				{"title", "Sample Card 3"},
+				{"text", "This is a sample flash card."},
+				{"answers", bson.A{
+					bson.D{
+						{"field", 3},
+						{"answers", bson.A{TESTANSWER3}},
+						{"questionType", Model.SingleAnswer},
+					},
+				}},
+				{"media", bson.A{"sample.jpg"}},
+				{"lang", bson.A{"en", "es"}},
+				{"topics", bson.A{"sample"}},
+				{"dateCreated", primitive.NewDateTimeFromTime(time.Now())},
+			},
+		}
+
+		find := mtest.CreateCursorResponse(
+			0,
+			fmt.Sprintf("%s.%s", mt.DB.Name(), TestDBName),
+			mtest.FirstBatch,
+			responses...)
+		mt.AddMockResponses(find)
+		stringIds := make([]string, 3)
+		for i := 0; i < 3; i++ {
+			stringIds[i] = primitive.ObjectID.Hex(primitiveIDs[i])
+		}
+
+		returnedCards, err := GetCardByIds(context.TODO(), mt.DB, stringIds)
+		if err != nil {
+			mt.Fatalf("Error found when getting cards by username: %s", err)
+		}
+
+		assert.Equal(t, len(responses), len(returnedCards))
+		for i, val := range returnedCards {
+			assert.Equal(t, val.Title, fmt.Sprintf("Sample Card %d", i+1))
+			assert.Equal(t, val.Text, "This is a sample flash card.")
+			assert.Equal(t, len(val.Answers), 1)
+			if i == 0 {
+				assert.Equal(t, val.Username, TESTUSERNAME1)
+				assert.Equal(t, []string{TESTANSWER1}, val.Answers[0].Answers)
+				assert.Equal(t, 1, val.Answers[0].Field)
+			}
+			if i == 1 {
+				assert.Equal(t, val.Username, TESTUSERNAME2)
+				assert.Equal(t, []string{TESTANSWER2}, val.Answers[0].Answers)
+				assert.Equal(t, 2, val.Answers[0].Field)
+			}
+			if i == 2 {
+				assert.Equal(t, val.Username, TESTUSERNAME3)
+				assert.Equal(t, []string{TESTANSWER3}, val.Answers[0].Answers)
+				assert.Equal(t, 3, val.Answers[0].Field)
+			}
+		}
+
+	})
+
+	mt.Run("get cards by nonexistent username", func(mt *mtest.T) {
+		nonExistentUsername := "NonExistentUser"
+
+		find := mtest.CreateCursorResponse(
+			0,
+			fmt.Sprintf("%s.%s", mt.DB.Name(), TestDBName),
+			mtest.FirstBatch)
+		mt.AddMockResponses(find)
+
+		returnedCards, err := GetCardsByUsername(context.TODO(), mt.DB, nonExistentUsername, 0)
+		if err != nil {
+			mt.Fatalf("Error found when getting cards by nonexistent username: %s", err)
+		}
+		assert.Equal(t, 0, len(returnedCards))
+	})
+
+}
+
+func TestGetCardsByUsername(t *testing.T) {
 	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
 	defer mt.Close()
 
@@ -218,5 +341,22 @@ func TestGetCardsByUsernameIntegration(t *testing.T) {
 				assert.Equal(t, 3, val.Answers[0].Field)
 			}
 		}
+	})
+
+	mt.Run("get cards by nonexistent username", func(mt *mtest.T) {
+		nonExistentUsername := "NonExistentUser"
+
+		find := mtest.CreateCursorResponse(
+			0,
+			fmt.Sprintf("%s.%s", mt.DB.Name(), TestDBName),
+			mtest.FirstBatch)
+		mt.AddMockResponses(find)
+
+		returnedCards, err := GetCardsByUsername(context.TODO(), mt.DB, nonExistentUsername, 0)
+		if err != nil {
+			mt.Fatalf("Error found when getting cards by nonexistent username: %s", err)
+		}
+
+		assert.Equal(t, 0, len(returnedCards))
 	})
 }
